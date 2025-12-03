e24f57225478ffed5576cb682ffb66791375eb9a
Amar <amarademi86@gmail.com>
2025-12-03 14:03:25 +0100

    Initial React setup

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductListPage from './pages/ProductListPage';
 import CartPage from './pages/CartPage';
 import User from './pages/User';
 import Location from './pages/Location';
-import { BrowserRouter } from 'react-router-dom';
 
 const AppRouter = () => {
   return (
     <BrowserRouter basename="/x-adv">
-    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductListPage />} />
@@ -18,7 +16,6 @@ const AppRouter = () => {
         <Route path="/user" element={<User />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
-    </Router>
     </BrowserRouter>
   );
 };
